9548c1fd42bc4009c4db4168c6fd87787823a1c7
MorzhaloPavel <morzhalo95@mail.ru>
2021-05-08 21:46:29 +0300

    feat: pipeline read write

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require("fs");
 const { Command } = require('commander');
 const program = new Command();
-const fs = require("fs");
+const { pipeline } = require('stream');
 
-const cipher = require("./cipher")
+const cipherCaesar = require("./cipher")
 
 const {caesarShift} = cipher
 
 
 program
-  .requiredOption('-s, --shift <numb>', 'a shift')
+.requiredOption('-s, --shift <number>', 'a shift')
   .option('-i, --input', 'an input file')
   .option('-o, --output', 'an output file')
   .requiredOption('-a, --action <encode/decode>', 'an action encode/decode')
@@ -23,45 +23,28 @@ program.parse(process.argv)
 
 const { action, shift, output, input } = program.opts();
 
+if (action !== 'decode' && action !== 'encode') {
+  process.stderr.write(
+    'Введите правильный "action"!'
+  );
+}
 
-const action = options.action
-let shift = +options.shift
-const shift = +options.shift
-const input = options.input
-const output = options.output
+// вернутся доделать проверку на число
 
+// if (typeof +shift !== 'number') {
+//   process.stderr.write(
+//     'Введите число!'
+//   );
+// }
 
+const readStream = input
+  ? fs.createReadStream('input.txt')
+  : process.stdin;
 
-caesarShift(stringW, shift)
+  const writeStream = output
+  ? fs.createWriteStream('output.txt', { flags: 'a+' })
+  : process.stdout;
 
-
-
-
-
-
-
-
-
-
-
-let stringW = "1"
-console.log(stringW);
-
-let readableStream = fs.createReadStream("input.txt", "utf8");
-readableStream.on("data", function(chunk){ 
-  chunk
-});
-
-console.log(stringW);
-
-
-
-// let writeableStream = fs.createWriteStream("hello.txt");
-// writeableStream.write("Привет мир!");
-// writeableStream.write("Продолжение записи \n");
-// writeableStream.end("Завершение записи");
-
-
-const stream = require('stream');
-
-stream.Transform 
+  pipeline(readStream, transform, writeStream, () => {
+    console.log('Pipeline succeeded.');
+  });
