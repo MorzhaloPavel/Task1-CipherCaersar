87288c0bc420c32981b0358bb9cab7f0957a77e7
MorzhaloPavel <morzhalo95@mail.ru>
2021-05-08 21:39:15 +0300

    feat: destructuring program

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const fs = require("fs");
 const { Command } = require('commander');
 const program = new Command();
 const fs = require("fs");
+
 const cipher = require("./cipher")
 
 const {caesarShift} = cipher
@@ -11,21 +13,55 @@ program
   .option('-i, --input', 'an input file')
   .option('-o, --output', 'an output file')
   .requiredOption('-a, --action <encode/decode>', 'an action encode/decode')
+  .option('-i, --input <filename>', 'an input file')
+  .option('-o, --output <filename>', 'an output file')
+  .requiredOption('-a, --action <type>', 'an action encode/decode')
   .parse();
 const options = program.opts();
+
+program.parse(process.argv)
+
+const { action, shift, output, input } = program.opts();
 
 
 const action = options.action
 let shift = +options.shift
+const shift = +options.shift
 const input = options.input
 const output = options.output
 
-if (action === 'encode') {
-  shift
-} else if (action === 'decode') {
-  shift = -shift
-} else {
-  return console.error('Введите правильный "action"!');
-}
+
 
 caesarShift(stringW, shift)
+
+
+
+
+
+
+
+
+
+
+
+let stringW = "1"
+console.log(stringW);
+
+let readableStream = fs.createReadStream("input.txt", "utf8");
+readableStream.on("data", function(chunk){ 
+  chunk
+});
+
+console.log(stringW);
+
+
+
+// let writeableStream = fs.createWriteStream("hello.txt");
+// writeableStream.write("Привет мир!");
+// writeableStream.write("Продолжение записи \n");
+// writeableStream.end("Завершение записи");
+
+
+const stream = require('stream');
+
+stream.Transform 
